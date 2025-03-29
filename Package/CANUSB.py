ecbcec726eb445c1d573311fb60ce30371a14bb4
sterwen <laurent.carre@sterwen-technology.eu>
2025-03-29 15:39:49 +0100

    Premièrs modification Laurent

--- Package/CANUSB.py
+++ Package/CANUSB.py
@@ -1,10 +1,6 @@
 import ctypes
 from ctypes import Structure, c_byte, c_ubyte,c_long, c_int, POINTER
 
-# ======================================================================
-# Charger la DLL
-canusb = ctypes.WinDLL("canusbdrv64.dll")
-
 class CanMsg(Structure):
     _fields_ = (
         ("ID", c_long),
@@ -14,20 +10,60 @@ class CanMsg(Structure):
         ("data", c_ubyte *8)
     )
 
-# =============== DEFINITION DES FONCTIONS =======================
-#
-# Fonction OPEN
-canusb.canusb_Open.restype = c_long  # Type de retour : entier long
-canusb_Open = canusb.canusb_Open
 
-# Fonction CLOSE
-# canusb.canusb_Close.restype=c_long
-canusb.canusb_Close.argtypes = [c_long]
-canusb_Close = canusb.canusb_Close
+class CanError(Exception):
+    pass
 
-# Fonction READ
-canusb_Read = canusb.canusb_Read
-canusb_Read.argtypes = [c_long, POINTER(CanMsg)]
-canusb_Read.restype = c_int
+
+class WindowsUSBCANInterface:
+
+    def __init__(self):
+    # ======================================================================
+    # Charger la DLL
+        try:
+            self._dll = ctypes.WinDLL("canusbdrv64.dll")
+        except (OSError, FileNotFoundError) as err:
+            print("Erreur DLL:", err)
+            raise CanError
+
+
+
+
+        # =============== DEFINITION DES FONCTIONS INTERFAC DLL=======================
+        #
+        # Fonction OPEN
+        self._dll.canusb_Open.restype = c_long  # Type de retour : entier long
+        # Fonction CLOSE
+        self._dll.canusb_Close.argtypes = [c_long]
+        # Fonction READ
+        self._dll.canusb_Read.argtypes = [c_long, POINTER(CanMsg)]
+        self._dll.canusb_Read.restype = c_int
+        self._handle = None
+
+
+    def open(self, bitrate=b"250", acceptance_code=0, acceptance_mask=0xFFFFFFFF, flags=1):
+        """
+        ouvre le canal d'accès au bus
+
+        :return:
+        rien. Exception CanError si problème
+        """
+        self._handle = self._dll.canusb_Open(None, bitrate, acceptance_code, acceptance_mask, flags)
+        if self._handle is None:
+            raise CanError("Erreur ouverture canal CAN")
+
+    def read(self) -> CanMsg:
+        if self._handle is None:
+           raise CanError("Channel not open")
+        msg = CanMsg()
+        result = self._dll.canusb_Read(self._handle, ctypes.byref(msg))
+        if result:
+            return msg
+        else:
+            raise CanError("Erreur sur lecture CAN")
+
+    def close(self):
+        self._dll.canusb_Close(self._handle)
+        self._handle = None
 
 # message=CanMsg
--- main.py
+++ main.py
@@ -1,183 +1,188 @@
 import tkinter as tk
-from tkinter import Checkbutton, BooleanVar,ttk
+from tkinter import Checkbutton, BooleanVar,ttk, filedialog
 from threading import Thread
-import ctypes
-from Package.CANUSB import canusb_Open, canusb_Read, CanMsg, canusb_Close
-from Package.Fonctions_USBCAN import enregistre, choix_fichier
+from Package.CANUSB import CanMsg, WindowsUSBCANInterface, CanError
 
-# Fonction de creation de la fenêtre principale
-def create_window_principal():
-    root = tk.Tk()
-    root.title("LECTURE DES TRAMES EN TEMPS REEL")
-    root.geometry("600x386")
+class MainWindow:
 
-    frame = tk.Frame(root)
-    frame.place(x=50, y=100)
+    # Fonction de creation de la fenêtre principale
+    def __init__(self):
+        self._root = tk.Tk()
+        self._root.title("LECTURE DES TRAMES EN TEMPS REEL")
+        self._root.geometry("600x386")
+
+        frame = tk.Frame(self._root)
+        frame.place(x=50, y=100)
+        self._can_interface = None
+        # créé l'interface avec le CAN
+
+        try:
+            self._can_interface = WindowsUSBCANInterface()
+        except CanError:
+            raise
+
+        self._stop_loop = False  # Réinitialise la condition d'arrêt
+        self._output_fd = None
+
+        # =============== ALJOUT DES CONTROLES SUR LA FENETRE ===============================
+        # Ajout d'un bouton OPEN et défini sa taille puis défini sont emplacement
+        self.button_open = tk.Button(self._root, text="Open", width=10, height=1, command=self.on_open_click)
+        # button.pack(pady=20)
+        self.button_open.place(x=10, y=40)
+
+        # Ajout d'un bouton CLOSE et défini sa taille puis défini sont emplacement
+        self.button_close = tk.Button(self._root, text="Close", width=10, height=1, command=self.on_close_click)
+        self.button_close.place(x=100, y=40)
+
+        # Ajout d'un bouton READ et défini sa taille puis défini sont emplacement
+        self.button_read = tk.Button(self._root, text="Read", width=10, height=1, command=self.on_read_click)
+        self.button_read.place(x=10, y=80)
+
+        button_stop = tk.Button(self._root, text="Arrêter", width=10, height=1, command=self.on_stop_click)
+        button_stop.pack()
+        button_stop.place(x=200, y=80)
+
+        # Ajput d'une CheckBox
+        self.check_enr = BooleanVar()
+        check_enregistre = tk.Checkbutton(self._root, text="Enregistrer", variable=self.check_enr, command=self.on_checkbox_change)
+        check_enregistre.place(x=100, y=80)  # Coordonnées précises en pixels
+
+        # Ajout un TreeView avec 3 colonnes
+        self.tree = ttk.Treeview(self._root, columns=("ID", "Length", "Data"), show="headings", height=18)
+        # Positionner le Treeview et la Scrollbar
+        self.tree.place(x=330, y=0)
+
+        # Ajout des en-têtes des colonnes
+        self.tree.heading("ID", text="ID")
+        self.tree.heading("Length", text="Len")
+        self.tree.heading("Data", text="Data")
+
+        # Ajuster la taille des colonnes
+        self.tree.column("ID", width=70)
+        self.tree.column("Length", width=30)
+        self.tree.column("Data", width=150)
+
+        # Ajout d'une verticale scrollbar sur la fenêtre
+        scrollbar = ttk.Scrollbar(self._root, orient='vertical', command=self.tree.yview)
+        self.tree.configure(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side='right', fill='y')
+
+        # Ajouter un label
+        self.label = tk.Label(self._root, text="Affichage du nombre de trames", width=40, anchor='w')
+        self.label.place(x=5, y=10)
+
+        # ====================== FIN DES CONTROLES ======================================
+
+    def open(self):
+        self._root.mainloop()
 
     # ========== FONCTIONS D'ACTIVATION ET DESACTIVATION DES BOUTONS ==========
-    def disable_button_open():
-        button_open['state'] = 'disabled'  # Désactive le bouton
+    def disable_button_open(self):
+        self.button_open['state'] = 'disabled'  # Désactive le bouton
 
-    def enable_button_open():
-        button_open['state'] = 'normal'  # Active le bouton
+    def enable_button_open(self):
+        self.button_open['state'] = 'normal'  # Active le bouton
 
-    def disable_button_read():
-        button_read['state'] = 'disabled'  # Désactive le bouton
+    def disable_button_read(self):
+        self.button_read['state'] = 'disabled'  # Désactive le bouton
 
-    def enable_button_read():
-        button_read['state'] = 'normal'  # Active le bouton
+    def enable_button_read(self):
+        self.button_read['state'] = 'normal'  # Active le bouton
     # =========================================================================
 
     # =================== FONCTIONS DES CONTROLES SUR LA FENETRE ======================
     # Fonction sur OPEN click
-    def on_open_click():
-        global handle  # Le handle est une variable global, utilisé par les autres fonctions
+    def on_open_click(self):
 
         print("Bouton cliqué ! Voici un programme d'ouverture.")
-        # Appeler la fonction
-        szID = None  # Exemple d'ID en bytes
-        szBitrate = b"250"  # Exemple de taux de transmission en bytes
-        acceptance_code = 0x0
-        acceptance_mask = 0xFFFFFFFF
-        flags = 0x1
 
         # Exécution de la fonction OPEN
-        handle = canusb_Open(szID, szBitrate, acceptance_code, acceptance_mask, flags)
-        print(f"Résultat de l'appel : {handle}")
-        if handle:                  # Si c'est ouvert
-           disable_button_open()    # Met le bouton d'ouverture en disabled
-           result=True
-        else:
-            print("PAS BON le HANDLE")
-
-    def on_stop_click():
-        global stop_loop
-        stop_loop = True  # Met fin à la boucle
-        enable_button_read()
-
-    def on_read_click():    # Lit en temps réel
-        print("Bouton cliqué ! Voici votre programme de lecture.")
         try:
-            # Vérifier si le handle est valide
-            if handle:
-                print("CA RECOIT")
-                # Créer une instance de la structure CanMsg
-                msg = CanMsg()
+            self._can_interface.open()
+        except CanError:
+            print("PAS BON le HANDLE")
+            return
+        self.disable_button_open()
 
-                global stop_loop    # Variable globale pour arrêter la boucle
-                stop_loop = False   # Réinitialise la condition d'arrêt
-                compteur = 0
-                disable_button_read()   # Desavtive le boutpn READ
-                # Appelle la fonction de lecture en temps réel
-                while not stop_loop:
-                    result = canusb_Read(handle, ctypes.byref(msg))
+    def on_stop_click(self):
+        self._stop_loop = True  # Met fin à la boucle
+        self.enable_button_read()
 
-                    # enregistre(msg, tree, check_enr)    # A SUPPRIMER *******************
-                    if result == 1:
-                        # Appel la fonction pour enregistrer et afficher sur la TreeView
-                        enregistre(msg,tree,check_enr)
-                        compteur += 1
-                        label.config(text=f"Compteur : {compteur}")
-                    else:
-                        """
-                        print(f"ID: {msg.ID}")
-                        print(f"TimeStamp: {msg.TimeStamp}")
-                        print(f"Flags: {msg.flags}")
-                        print(f"Length: {msg.len}")
-                        print("Data:", list(msg.data))
-                        """
+    def on_read_click(self):    # Lit en temps réel
+        print("Bouton cliqué ! Voici votre programme de lecture.")
+        compteur = 0
+        self._stop_loop = False
+        self.disable_button_read()   # Desavtive le boutpn READ
+        # Appelle la fonction de lecture en temps réel
+        while not self._stop_loop:
+            try:
+                msg = self._can_interface.read()
+                self.enregistre(msg)
+                compteur += 1
+                self.label.config(text=f"Compteur : {compteur}")
+                self._root.update()  # Donne la main au systême
+            except CanError as err:
+                print(err)
+                break
+            except Exception as err:   # catch all => à enlever
+                print("Exception:", err)
+                break
 
-                    root.update()   # Donne la main au systême
-
-                stop_loop=True
+        self.enable_button_read()
 
 
-        except Exception as e:
-            print(f"Exception sur le handle : {e}")
-
-    def on_checkbox_change():
-        if check_enr.get():  # Si la case est cochée
+    def on_checkbox_change(self):
+        if self.check_enr.get():  # Si la case est cochée
             print("Checkbox cochée - Enregistrement activé")
             # Pour récupérer le chemin et créer un nouveau fichier
-            fichier_chemin = choix_fichier()
+            self.choix_fichier()
 
         else:  # Si la case est décochée
-            fichier_chemin = None
+            self.close_file()
+            self.fichier_chemin = None
             print("Checkbox décochée - Enregistrement désactivé")
 
-    def on_close_click():
-        on_stop_click()         # Arrête le boucle
-        canusb_Close(handle)    # Ferme l'adaptateur
-        enable_button_open()    # Active le bouron d'ouverture
+    def on_close_click(self):
+        self.on_stop_click()         # Arrête le boucle
+        self._can_interface.close()    # Ferme l'adaptateur
+        self._output_fd.close()
+        self.enable_button_open()    # Active le bouron d'ouverture
         result = False
+
+    def choix_fichier(self):
+        # Ouvrir une boîte de dialogue pour sélectionner un fichier
+        self.fichier_chemin = filedialog.asksaveasfilename(
+            title="Choisissez le fichier à enregistrer",
+            filetypes=(("Fichiers texte", "*.txt"), ("Tous les fichiers", "*.*")),
+            defaultextension=".txt"
+        )
+
+    def open_file(self):
+        if self.fichier_chemin is not None:
+            try:
+                self._output_fd = open(self.fichier_chemin, "a")  # Créer le nouveau fichier
+            except IOError as err:
+                print(f"Erreur sur fichier {self.fichier_chemin}: {err}")
+
+    def enregistre(self, msg: CanMsg):
+        if self.check_enr and self._output_fd is not None:
+            self._output_fd.write(f"{msg.TimeStamp} {msg.Id:08X} {msg.data.hex(" ")}\n")
+
+    def close_file(self):
+        if self._output_fd is not None:
+            self._output_fd.close()
+            self._output_fd = None
+
+
 
     # ==================================================================================
 
-    # =============== ALJOUT DES CONTROLES SUR LA FENETRE ===============================
-    # Ajout d'un bouton OPEN et défini sa taille puis défini sont emplacement
-    button_open = tk.Button(root, text="Open", width=10, height=1, command=on_open_click)
-    # button.pack(pady=20)
-    button_open.place(x=10, y=40)
 
-    # Ajout d'un bouton CLOSE et défini sa taille puis défini sont emplacement
-    button_close = tk.Button(root, text="Close", width=10, height=1, command=on_close_click)
-    button_close.place(x=100, y=40)
-
-    # Ajout d'un bouton READ et défini sa taille puis défini sont emplacement
-    button_read = tk.Button(root, text="Read", width=10, height=1, command=on_read_click)
-    button_read.place(x=10, y=80)
-
-    button_stop = tk.Button(root, text="Arrêter", width=10, height=1, command=on_stop_click)
-    button_stop.pack()
-    button_stop.place(x=200, y=80)
-
-
-    # Ajput d'une CheckBox
-    check_enr = BooleanVar()
-    check_enregistre = tk.Checkbutton(root,text="Enregistrer",variable=check_enr,command=on_checkbox_change)
-    check_enregistre.place(x=100, y=80)  # Coordonnées précises en pixels
-
-    # Ajout un TreeView avec 3 colonnes
-    tree = ttk.Treeview(root, columns=("ID", "Length", "Data"), show="headings", height=18)
-    # Positionner le Treeview et la Scrollbar
-    tree.place(x=330, y=0)
-
-    # Ajout des en-têtes des colonnes
-    tree.heading("ID", text="ID")
-    tree.heading("Length", text="Len")
-    tree.heading("Data", text="Data")
-
-    # Ajuster la taille des colonnes
-    tree.column("ID", width=70)
-    tree.column("Length", width=30)
-    tree.column("Data", width=150)
-
-    # Ajout d'une verticale scrollbar sur la fenêtre
-    scrollbar = ttk.Scrollbar(root, orient='vertical', command=tree.yview)
-    tree.configure(yscrollcommand=scrollbar.set)
-    scrollbar.pack(side='right', fill='y')
-
-
-    # Ajouter un label
-    label = tk.Label(root, text="Affichage du nombre de trames", width=40 , anchor='w')
-    label.place(x=5, y=10)
-
-    # ====================== FIN DES CONTROLES ======================================
-
-
-    # ==============================================================================
-    # Démarrer la boucle principale de l'interface graphique
-    root.mainloop()
-    # ==============================================================================
-
-# ================= DEMARRAGE DU THREAD ===========================================
-def run_thread():
-    # Créer et démarrer un thread pour la fenêtre
-    thread = Thread(target=create_window_principal)
-    thread.start()
 
 # Lance le Thread
 if __name__ == "__main__":
-    run_thread()
+    main_window = MainWindow()
+    main_window.open()
+
 # =================================================================================
 
